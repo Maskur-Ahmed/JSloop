b6ae325c03223395e4b4785f8e564beaf039d0d0
Maskur Ahmed <maskurahmed37@gmail.com>
2025-09-07 01:05:11 +0600

    task two complete

--- js/for.js
+++ js/for.js
@@ -4,8 +4,35 @@
 //     console.log(num);
 // }
 
-//* task one:
+//* task 1:
 
 // for(i = 1; i<=60; i++){
 //     console.log("I will invest at least 6 hrs every single day for next 60 days",i);
 // }
+
+//* task 2:
+// subtask 1:
+
+// for(n =61 ; n<=100; n++){
+//     // console.log(n);
+//     if(n % 2 === 1){
+//         console.log(n);
+//     }
+// }
+
+//subtask 2:
+
+// for(n=78; n<=98; n++){
+//     if(n%2 ===  0){
+//         console.log(n);
+//     }
+// }
+
+//* task 3:
+let sum = 0;
+for(let n=91; n<=129; n++){
+   if(n%2===1){
+    sum = sum+n
+    console.log(sum);
+   }
+}
