2d1bba6f1b6d2f8ba8d844dbccb5cb9a75f8f5c9
Maskur Ahmed <maskurahmed37@gmail.com>
2025-09-07 00:53:31 +0600

    task one complete

--- js/for.js
+++ js/for.js
@@ -1,5 +1,11 @@
 //syntax:
 
-for(let num = 1; num<=10; num++){
-    console.log(num);
-}
+// for(let num = 1; num<=10; num++){
+//     console.log(num);
+// }
+
+//* task one:
+
+// for(i = 1; i<=60; i++){
+//     console.log("I will invest at least 6 hrs every single day for next 60 days",i);
+// }
