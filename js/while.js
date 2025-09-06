5ffb44888c89a9bcb3a650a22e99dbbc3f3cd1c6
Maskur Ahmed <maskurahmed37@gmail.com>
2025-09-06 11:13:19 +0600

    task 2 complete

--- js/while.js
+++ js/while.js
@@ -1,6 +1,19 @@
+//*task one :
 
-let num = 1;
-while(num<=60){
-    console.log("I will invest at least 6 hrs every single day for next 60 days!",num)
-    num++;
+// let num = 1;
+// while(num<=60){
+//     console.log("I will invest at least 6 hrs every single day for next 60 days!",num)
+//     num++;
+// }
+
+//*task 2 :
+
+let num = 61;
+while(num<=100){
+    console.log(num);
+    if(num % 2 === 1){
+        console.log('this is odd number',num )
+    }
+    num++
 }
+
