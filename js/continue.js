3e6859ea4777554952d7af2f50cca08ea25053ad
Maskur Ahmed <maskurahmed37@gmail.com>
2025-09-08 04:07:02 +0600

    very tricky question solved

--- js/continue.js
+++ js/continue.js
@@ -4,5 +4,14 @@ for(i=1;i<=40;i++){
   if(i%2 !==0){
     continue;
   }
-  console.log(i);
+//   console.log(i);
+}
+
+//* task 2:
+
+for(n=55;n<=85;n++){
+    if( n%2===1 && n%5 !==0){
+        console.log('it can divisible by 5',n)
+        continue;
+    }
 }
